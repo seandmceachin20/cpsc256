8d0fc3623c0bb0ad84bed99053a0d76c4102d74b
Edward Brash <brash@jlab.org>
2023-08-13 15:48:36 -0400

    Updates

--- cpsc256/cmake-build-debug/cmake_install.cmake
+++ cpsc256/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/brash/CLionProjects/CPSC256
+# Install script for directory: /Users/brash/CLionProjects/cpsc256
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/brash/CLionProjects/CPSC256/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/brash/CLionProjects/cpsc256/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
